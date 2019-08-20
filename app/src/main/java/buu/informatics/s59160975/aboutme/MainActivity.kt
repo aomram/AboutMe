97f4a0850e141e80180c016d547bc5621a6662c6
Ramnaree Mailaem <59160975@go.buu.ac.th>
2019-08-21 01:02:25 +0700

    Binding 4

--- app/src/main/java/buu/informatics/s59160975/aboutme/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160975/aboutme/MainActivity.kt
@@ -14,6 +14,7 @@ import buu.informatics.s59160975.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val myName:MyName = MyName("Ramnaree")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -25,16 +26,17 @@ class MainActivity : AppCompatActivity() {
             nicknameText.setOnClickListener {
                 updateNickname(it)
             }
+            this.myName = this@MainActivity.myName
         }
 
     }
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
-
+            invalidateAll()
             // Hide the keyboard.
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
